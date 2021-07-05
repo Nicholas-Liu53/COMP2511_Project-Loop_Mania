864182fe088cb4e29535b60e7d263f8ccc68659c
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-07-05 17:09:31 +1000

    Started on itemstests.java

---
+++ src/test/ItemsTests.java
@@ -0,0 +1,99 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import unsw.loopmania.*;
+import unsw.loopmania.items.*;
+
+
+public class ItemsTests {
+    
+    @Test
+    public void healthPotionTest() {
+        /* 
+            Requirements to test:
+             1. Obtainable through purchase in the shop
+             2. Obtainable through picking up off the ground
+             3. Purchasing price is 500 gold
+             4. Selling price is 240 gold
+        */
+        
+        // Testing Requirement 1:
+    } 
+
+    @Test 
+    public void bodyArmourTest() {
+        /* 
+            Requirements to test:
+             1. Obtainable through purchase in the shop
+             2. Purchasing price is 500 gold
+             3. Selling price is 400 gold
+        */
+        
+        // Testing Requirement 1:
+    }
+
+    @Test 
+    public void helmetTest() {
+        /* 
+            Requirements to test:
+             1. Obtainable through purchase in the shop
+             2. Purchasing price is 200 gold
+             3. Selling price is 160 gold
+        */
+        
+        // Testing Requirement 1:
+    }
+
+    @Test 
+    public void shieldTest() {
+        /* 
+            Requirements to test:
+             1. Obtainable through purchase in the shop
+             2. Purchasing price is 300 gold
+             3. Selling price is 240 gold
+        */
+        
+        // Testing Requirement 1:
+    }
+
+    @Test
+    public void swordTest() {
+        /* 
+            Requirements to test:
+             1. Obtainable through purchase in the shop
+             2. Purchasing price is 200 gold
+             3. Selling price is 160 gold
+        */
+        
+        // Testing Requirement 1:
+    }
+    
+    @Test
+    public void staffTest() {
+        /* 
+            Requirements to test:
+             1. Obtainable through purchase in the shop
+             2. Purchasing price is 300 gold
+             3. Selling price is 240 gold
+        */
+        
+        // Testing Requirement 1:
+    }
+
+    @Test
+    public void stakeTest() {
+        /* 
+            Requirements to test:
+             1. Obtainable through purchase in the shop
+             2. Purchasing price is 150 gold
+             3. Selling price is 120 gold
+        */
+        
+        // Testing Requirement 1:
+    }
+}
