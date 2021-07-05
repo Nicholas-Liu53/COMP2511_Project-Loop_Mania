b7dda988f35d436c42b05b5cb4f48256352bbfdc
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-07-05 16:29:04 +1000

    Added stub test code for buildingtests

--- src/test/BuildingsTests.java
+++ src/test/BuildingsTests.java
@@ -14,6 +14,16 @@ public class BuildingsTests {
     
     @Test
     public void herosCastleTest() {
+        /* 
+            Requirements to test:
+             1. Character gotta start there --> assert that position is at the beginning of the path
+             2. Shop is opened when hero arrives at castle
+             3. There is only one hero's castle and it's there
+        */
+        
+        // Testing Requirement 1:
+        // Testing Requirement 2:
+        // Testing Requirement 3:
         
     } 
 }
