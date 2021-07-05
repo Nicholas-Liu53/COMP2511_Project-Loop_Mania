9d83562ffc53746b3cc2bc50470bb5e4a020ab70
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-07-05 16:11:12 +1000

    added testing file for buildings

---
+++ src/test/BuildingsTests.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import unsw.loopmania.*;
+import unsw.loopmania.buildings.*;
+
+
+public class BuildingsTests {
+    
+    @Test
+    public void herosCastleTest() {
+        
+    } 
+}
