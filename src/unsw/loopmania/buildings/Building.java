03f71ff4713d578e5e549d85102b26bb860db5df
Cyrus Wilkie <james.wilkiec@gmail.com>
2021-07-09 15:56:35 +1000

    implemented WorldStateObserver in buildings

--- src/unsw/loopmania/buildings/Building.java
+++ src/unsw/loopmania/buildings/Building.java
@@ -1,15 +1,25 @@
 package unsw.loopmania.buildings;
 
 import javafx.beans.property.SimpleIntegerProperty;
+import unsw.loopmania.LoopManiaWorld;
 import unsw.loopmania.StaticEntity;
+import unsw.loopmania.WorldStateObserver;
 
 /**
  * a Building in the world
  * which doesn't move
  */
-public abstract class Building extends StaticEntity {
+public abstract class Building extends StaticEntity implements WorldStateObserver {
     // TODO = implement other varieties of card than VampireCastleCard
     public Building(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
+    }
+
+    /**
+     * Implementing WorldStateObserver, by default a building does nothing
+     * when notified
+     */
+    public void notify(LoopManiaWorld worldState) {
+        return;
     }
 }
