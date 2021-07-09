0fe91d7ca9d6c3ce87dcbfb11ebea1b080863c0f
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2021-07-09 16:39:46 +0530

    added missing break statements

--- src/unsw/loopmania/LoopManiaWorld.java
+++ src/unsw/loopmania/LoopManiaWorld.java
@@ -492,28 +492,6 @@ public class LoopManiaWorld {
      * @return list of enemies which have been killed
      */
     // public List<Enemy> runBattles() {
-    // // TODO = modify this - currently the character automatically wins all
-    // battles without any damage!
-    // List<Enemy> defeatedEnemies = new ArrayList<Enemy>();
-    // for (Enemy e: enemies){
-    // // Pythagoras: a^2+b^2 < radius^2 to see if within radius
-    // // TODO = you should implement different RHS on this inequality, based on
-    // influence radii and battle radii
-    // if (Math.pow((character.getX()-e.getX()), 2) +
-    // Math.pow((character.getY()-e.getY()), 2) < 4){
-    // // fight...
-    // defeatedEnemies.add(e);
-    // }
-    // }
-    // for (Enemy e: defeatedEnemies){
-    // // IMPORTANT = we kill enemies here, because killEnemy removes the enemy from
-    // the enemies list
-    // // if we killEnemy in prior loop, we get
-    // java.util.ConcurrentModificationException
-    // // due to mutating list we're iterating over
-    // killEnemy(e);
-    // }
-    // return defeatedEnemies;
     // }
 
     // *-------------------------------------------------------------------------
@@ -524,21 +502,6 @@ public class LoopManiaWorld {
      * 
      * @return a card to be spawned in the controller as a JavaFX node
      */
-    // public VampireCastleCard loadVampireCard() {
-    // // if adding more cards than have, remove the first card...
-    // if (cardEntities.size() >= getWidth()) {
-    // // TODO = give some cash/experience/item rewards for the discarding of the
-    // // oldest card
-    // giveRandomRewards("noCard");
-    // removeCard(0);
-    // }
-    // VampireCastleCard vampireCastleCard = new VampireCastleCard(new
-    // SimpleIntegerProperty(cardEntities.size()),
-    // new SimpleIntegerProperty(0));
-    // cardEntities.add(vampireCastleCard);
-    // return vampireCastleCard;
-    // }
-
     public Card loadCard(String cardType) {
         // if adding more cards than have, remove the first card...
         if (this.cardEntities.size() >= this.getWidth()) {
@@ -552,20 +515,29 @@ public class LoopManiaWorld {
         switch (cardType) {
             case "BarracksCard":
                 card = new BarracksCard(new SimpleIntegerProperty(cardEntities.size()), new SimpleIntegerProperty(0));
+                break;
             case "CampfireCard":
                 card = new CampfireCard(new SimpleIntegerProperty(cardEntities.size()), new SimpleIntegerProperty(0));
+                break;
             case "TowerCard":
                 card = new TowerCard(new SimpleIntegerProperty(cardEntities.size()), new SimpleIntegerProperty(0));
+                break;
             case "TrapCard":
                 card = new TrapCard(new SimpleIntegerProperty(cardEntities.size()), new SimpleIntegerProperty(0));
+                break;
             case "VillageCard":
                 card = new VillageCard(new SimpleIntegerProperty(cardEntities.size()), new SimpleIntegerProperty(0));
+                break;
             case "ZombiePitCard":
                 card = new ZombiePitCard(new SimpleIntegerProperty(cardEntities.size()), new SimpleIntegerProperty(0));
+                break;
             case "VampireCastleCard":
                 card = new VampireCastleCard(new SimpleIntegerProperty(cardEntities.size()),
                         new SimpleIntegerProperty(0));
+            default:
+                break;
         }
+
         cardEntities.add(card);
         return card;
     }
