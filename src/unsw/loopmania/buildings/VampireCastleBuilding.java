5694ecc6f5d60e5e7459504a0b782ff94accacc9
Cyrus Wilkie <james.wilkiec@gmail.com>
2021-07-09 15:57:03 +1000

    implemented notify prototype for vampire castle

--- src/unsw/loopmania/buildings/VampireCastleBuilding.java
+++ src/unsw/loopmania/buildings/VampireCastleBuilding.java
@@ -2,13 +2,22 @@ package unsw.loopmania.buildings;
 
 import javafx.beans.property.SimpleIntegerProperty;
 // import unsw.loopmania.StaticEntity;
+import unsw.loopmania.LoopManiaWorld;
+import unsw.loopmania.enemies.VampireEnemy;
 
 /**
  * a basic form of building in the world
  */
 public class VampireCastleBuilding extends Building {
-    // TODO = add more types of building, and make sure buildings have effects on entities as required by the spec
     public VampireCastleBuilding(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
+    }
+
+    @Override
+    public void notify(LoopManiaWorld worldState) {
+        if (worldState.getCurrCycle() % 5 == 0) {
+            // TODO must figure out location to spawn
+            worldState.addNewEnemy(new VampireEnemy(null));
+        }
     }
 }
