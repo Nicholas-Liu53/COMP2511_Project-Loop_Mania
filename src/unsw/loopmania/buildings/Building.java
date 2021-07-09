e38404192af7aac5061252d71ca78cad7a9a58f3
Cyrus Wilkie <james.wilkiec@gmail.com>
2021-07-09 17:24:32 +1000

    added GetAdjacentPathTile method

--- src/unsw/loopmania/buildings/Building.java
+++ src/unsw/loopmania/buildings/Building.java
@@ -1,9 +1,13 @@
 package unsw.loopmania.buildings;
 
+import java.util.List;
+import org.javatuples.Pair;
+
 import javafx.beans.property.SimpleIntegerProperty;
 import unsw.loopmania.LoopManiaWorld;
 import unsw.loopmania.StaticEntity;
 import unsw.loopmania.WorldStateObserver;
+import unsw.loopmania.path.PathPosition;
 
 /**
  * a Building in the world
@@ -22,4 +26,37 @@ public abstract class Building extends StaticEntity implements WorldStateObserve
     public void notify(LoopManiaWorld worldState) {
         return;
     }
+
+    /**
+     * Finds a path tile that is adjacent to this building's position
+     * @param orderedPath
+     * @return PathPosition if an adjacent tile is found, otherwise null
+     */
+    public PathPosition getAdjacentPathTile(List<Pair<Integer, Integer>> orderedPath) {
+        int x = super.getX() - 1;
+        int y = super.getY() - 1;
+
+        int xIterator = 0;
+        // Iterating through x
+        while (xIterator < 3) {
+            int yIterator = 0;
+            // Iterating through y
+            while (yIterator < 3) {
+                // Iterating through path to find relevant tile
+                int pairIterator = 0;
+                for (Pair<Integer, Integer> pair : orderedPath) {
+                    if (pair.getValue0() == x + xIterator && pair.getValue1() == y + yIterator) {
+                        // Adjacent tile found, returning path position at that tile
+                        return new PathPosition(pairIterator, orderedPath);
+                    }
+                    pairIterator++;
+                }
+                yIterator++;
+            }
+            xIterator++;
+        }
+
+        return null;
+    }
+    
 }
