df711970faa7109493d8f56458cefb0522066221
Cyrus Wilkie <james.wilkiec@gmail.com>
2021-07-10 11:48:12 +1000

    implemented launchAttack in Stake

--- src/unsw/loopmania/items/Stake.java
+++ src/unsw/loopmania/items/Stake.java
@@ -2,13 +2,13 @@ package unsw.loopmania.items;
 
 import javafx.beans.property.SimpleIntegerProperty;
 import unsw.loopmania.StaticEntity;
+import unsw.loopmania.enemies.Enemy;
+import unsw.loopmania.enemies.VampireEnemy;
 
 /**
  * represents an equipped or unequipped stake in the backend world
  */
-public class Stake extends Item {
-    
-    private int vampireCrit = 15;
+public class Stake extends Item implements WeaponStrategy {
 
     public Stake(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y, "Weapon");
@@ -21,7 +21,12 @@ public class Stake extends Item {
         sellPrice = 120;
     }
     
-    public int getVampireCrit() { 
-        return vampireCrit; 
+    public void launchAttack(Enemy enemy, int baseDamage) {
+        if (enemy instanceof VampireEnemy) {
+            // Greater damage to vampires
+            enemy.receiveAttack(baseDamage + 15);
+        } else {
+            enemy.receiveAttack(baseDamage + 5);
+        }
     }
 }
