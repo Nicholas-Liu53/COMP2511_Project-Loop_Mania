fda7bf892af8010feca386f24b12ad4c1ed608bf
Cyrus Wilkie <james.wilkiec@gmail.com>
2021-07-10 11:44:23 +1000

    implemented WeaponStrategy in sword

--- src/unsw/loopmania/items/Sword.java
+++ src/unsw/loopmania/items/Sword.java
@@ -2,11 +2,12 @@ package unsw.loopmania.items;
 
 import javafx.beans.property.SimpleIntegerProperty;
 import unsw.loopmania.StaticEntity;
+import unsw.loopmania.enemies.Enemy;
 
 /**
  * represents an equipped or unequipped sword in the backend world
  */
-public class Sword extends Item {
+public class Sword extends Item implements WeaponStrategy {
     public Sword(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y, "Weapon");
         purchasePrice = 200;
@@ -16,5 +17,10 @@ public class Sword extends Item {
         super(new SimpleIntegerProperty(1), new SimpleIntegerProperty(2), "Weapon");
         purchasePrice = 200;
         sellPrice = 160;
-    }     
+    }
+    
+    public void launchAttack(Enemy enemy, int baseDamage) {
+        // Simply adds 10 extra damage
+        enemy.receiveAttack(baseDamage + 10);
+    }
 }
