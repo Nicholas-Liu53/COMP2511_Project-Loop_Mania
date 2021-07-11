fd9167fb63cf9afc2b465004574e2ee000b95935
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-07-11 23:47:31 +1000

    Added stuff into SMController

--- src/unsw/loopmania/ShopMenuController.java
+++ src/unsw/loopmania/ShopMenuController.java
@@ -1,7 +1,40 @@
 package unsw.loopmania;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
 
 /**
  * controller for the shop menu.
@@ -13,10 +46,144 @@ public class ShopMenuController {
      */
     private MenuSwitcher gameSwitcher;
 
+    @FXML
+    private GridPane unequippedInventory;
+
     public void setGameSwitcher(MenuSwitcher gameSwitcher){
         this.gameSwitcher = gameSwitcher;
     }
 
+    // All image views including tiles, character, enemies, cards... even though
+    // cards in separate gridpane...
+    private List<ImageView> entityImages;
+
+    private LoopManiaWorld world;
+
+    // Items
+    private Image bodyArmourImage;
+    private Image healthPotionImage;
+    private Image helmetImage;
+    private Image shieldImage;
+    private Image staffImage;
+    private Image stakeImage;
+    private Image swordImage;
+    private Image goldPileImage;
+    private Image theOneRingImage;
+
+    /**
+     * the image currently being dragged, if there is one, otherwise null. Holding
+     * the ImageView being dragged allows us to spawn it again in the drop location
+     * if appropriate.
+     */
+    // TODO = it would be a good idea for you to instead replace this with the
+    // building/item which should be dropped
+    private ImageView currentlyDraggedImage;
+
+    /**
+     * null if nothing being dragged, or the type of item being dragged
+     */
+    private DRAGGABLE_TYPE currentlyDraggedType;
+
+    /**
+     * squares gridpane includes path images, enemies, character, empty grass,
+     * buildings
+     */
+    @FXML
+    private GridPane squares;
+
+    /**
+     * cards gridpane includes cards and the ground underneath the cards
+     */
+    @FXML
+    private GridPane cards;
+
+    /**
+     * anchorPaneRoot is the "background". It is useful since anchorPaneRoot
+     * stretches over the entire game world, so we can detect dragging of
+     * cards/items over this and accordingly update DragIcon coordinates
+     */
+    @FXML
+    private AnchorPane anchorPaneRoot;
+
+    /**
+     * equippedItems gridpane is for equipped items (e.g. swords, shield, axe)
+     */
+    @FXML
+    private GridPane equippedItems;
+
+
+    /**
+     * when we drag a card/item, the picture for whatever we're dragging is set here
+     * and we actually drag this node
+     */
+    private DragIcon draggedEntity;
+
+    /**
+     * mapping from draggable type enum CARD/TYPE to the event handler triggered
+     * when the draggable type is dropped over its appropriate gridpane
+     */
+    private EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>> gridPaneSetOnDragDropped;
+    /**
+     * mapping from draggable type enum CARD/TYPE to the event handler triggered
+     * when the draggable type is dragged over the background
+     */
+    private EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>> anchorPaneRootSetOnDragOver;
+    /**
+     * mapping from draggable type enum CARD/TYPE to the event handler triggered
+     * when the draggable type is dropped in the background
+     */
+    private EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>> anchorPaneRootSetOnDragDropped;
+    /**
+     * mapping from draggable type enum CARD/TYPE to the event handler triggered
+     * when the draggable type is dragged into the boundaries of its appropriate
+     * gridpane
+     */
+    private EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>> gridPaneNodeSetOnDragEntered;
+    /**
+     * mapping from draggable type enum CARD/TYPE to the event handler triggered
+     * when the draggable type is dragged outside of the boundaries of its
+     * appropriate gridpane
+     */
+    private EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>> gridPaneNodeSetOnDragExited;
+
+    public ShopMenuController(LoopManiaWorld world, List<ImageView> initialEntities) {
+        this.world = world;
+        entityImages = new ArrayList<>(initialEntities);
+
+        // Items
+        bodyArmourImage = new Image((new File("src/images/armour.png")).toURI().toString());
+        healthPotionImage = new Image((new File("src/images/brilliant_blue_new.png")).toURI().toString());
+        helmetImage = new Image((new File("src/images/helmet.png")).toURI().toString());
+        shieldImage = new Image((new File("src/images/shield.png")).toURI().toString());
+        staffImage = new Image((new File("src/images/staff.png")).toURI().toString());
+        stakeImage = new Image((new File("src/images/stake.png")).toURI().toString());
+        swordImage = new Image((new File("src/images/basic_sword.png")).toURI().toString());
+        goldPileImage = new Image((new File("src/images/gold_pile.png")).toURI().toString());
+        theOneRingImage = new Image((new File("src/images/the_one_ring.png")).toURI().toString());
+        currentlyDraggedImage = null;
+        currentlyDraggedType = null;
+
+        // Initialize them all...
+        gridPaneSetOnDragDropped = new EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>>(DRAGGABLE_TYPE.class);
+        anchorPaneRootSetOnDragOver = new EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>>(DRAGGABLE_TYPE.class);
+        anchorPaneRootSetOnDragDropped = new EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>>(DRAGGABLE_TYPE.class);
+        gridPaneNodeSetOnDragEntered = new EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>>(DRAGGABLE_TYPE.class);
+        gridPaneNodeSetOnDragExited = new EnumMap<DRAGGABLE_TYPE, EventHandler<DragEvent>>(DRAGGABLE_TYPE.class);
+    }
+
+    @FXML
+    public void initialize() {
+        Image inventorySlotImage = new Image((new File("src/images/empty_slot.png")).toURI().toString());
+        Rectangle2D imagePart = new Rectangle2D(0, 0, 32, 32);
+        
+        // Add the empty slot images for the unequipped inventory
+        for (int x = 0; x < LoopManiaWorld.unequippedInventoryWidth; x++) {
+            for (int y = 0; y < LoopManiaWorld.unequippedInventoryHeight; y++) {
+                ImageView emptySlotView = new ImageView(inventorySlotImage);
+                unequippedInventory.add(emptySlotView, x, y);
+            }
+        }
+    }
     /**
      * facilitates switching to main game upon button click
      * @throws IOException
