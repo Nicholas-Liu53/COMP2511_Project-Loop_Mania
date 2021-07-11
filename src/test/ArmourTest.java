4a49b96f6be4c91175643e1477e5f3b0af291498
CyrusWilkie <james.wilkiec@gmail.com>
2021-07-11 21:10:57 +1000

    added tests for shield

--- src/test/ArmourTest.java
+++ src/test/ArmourTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 import unsw.loopmania.Character;
 import unsw.loopmania.enemies.Enemy;
 import unsw.loopmania.enemies.SlugEnemy;
+import unsw.loopmania.enemies.VampireEnemy;
 import unsw.loopmania.items.BodyArmour;
 import unsw.loopmania.items.BodyArmourStrategy;
+import unsw.loopmania.items.Shield;
+import unsw.loopmania.items.ShieldStrategy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,5 +34,26 @@ public class ArmourTest {
         assertEquals(85, mainChar.getHealth());
         mainChar.receiveAttack(50);
         assertEquals(60, mainChar.getHealth());
+    }
+
+    @Test
+    public void ShieldTest() {
+        ShieldStrategy shield = new Shield();
+        Character mainChar = new Character(null);
+
+        mainChar.equipItem(shield);
+        // Testing only shield
+        mainChar.receiveAttack(15);
+        assertEquals(88, mainChar.getHealth());
+        mainChar.receiveAttack(30);
+        assertEquals(64, mainChar.getHealth());
+        // Testing against vampire attack
+        for (int i = 0; i < 100; i++) {
+            Character newChar = new Character(null);
+            VampireEnemy enemy = new VampireEnemy(null);
+            mainChar.equipItem(shield);
+
+            enemy.launchAttack(newChar);
+        }
     }
 }
