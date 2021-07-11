4614cc1db9465dc9602a9f85041f9eedecef056c
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-07-11 17:49:28 +1000

    Modelled ShopMenuController after MainMenuController

--- src/unsw/loopmania/ShopMenuController.java
+++ src/unsw/loopmania/ShopMenuController.java
@@ -1,5 +1,28 @@
 package unsw.loopmania;
 
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+/**
+ * controller for the shop menu.
+ * 
+ */
 public class ShopMenuController {
-    
+    /**
+     * facilitates switching to main game
+     */
+    private MenuSwitcher gameSwitcher;
+
+    public void setGameSwitcher(MenuSwitcher gameSwitcher){
+        this.gameSwitcher = gameSwitcher;
+    }
+
+    /**
+     * facilitates switching to main game upon button click
+     * @throws IOException
+     */
+    @FXML
+    private void switchToGame() throws IOException {
+        gameSwitcher.switchMenu();
+    }
 }
