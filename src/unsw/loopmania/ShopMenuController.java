9a3ea7414ebe3acd300340b93f4e010c43125feb
samsolichin <s.solichin@student.unsw.edu.au>
2021-07-12 17:16:28 +1000

    fixed comments;

--- src/unsw/loopmania/ShopMenuController.java
+++ src/unsw/loopmania/ShopMenuController.java
@@ -172,13 +172,13 @@ public class ShopMenuController {
         // Image inventorySlotImage = new Image((new File("src/images/empty_slot.png")).toURI().toString());
         // Rectangle2D imagePart = new Rectangle2D(0, 0, 32, 32);
         
-        // // // Add the empty slot images for the unequipped inventory
-        // // for (int x = 0; x < LoopManiaWorld.unequippedInventoryWidth; x++) {
-        // //     for (int y = 0; y < LoopManiaWorld.unequippedInventoryHeight; y++) {
-        // //         ImageView emptySlotView = new ImageView(inventorySlotImage);
-        // //         unequippedInventory.add(emptySlotView, x, y);
-        // //     }
-        // // }
+        // // Add the empty slot images for the unequipped inventory
+        // for (int x = 0; x < LoopManiaWorld.unequippedInventoryWidth; x++) {
+        //     for (int y = 0; y < LoopManiaWorld.unequippedInventoryHeight; y++) {
+        //         ImageView emptySlotView = new ImageView(inventorySlotImage);
+        //         unequippedInventory.add(emptySlotView, x, y);
+        //     }
+        // }
 
         // // Create the draggable icon
         // draggedEntity = new DragIcon();
