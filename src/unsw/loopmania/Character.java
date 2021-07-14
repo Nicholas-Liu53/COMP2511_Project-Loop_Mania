6afafff5d4130fbaaa3a9436d236dc7971c70033
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2021-07-15 03:01:53 +0530

    adding this keyword to Character class' attributes

--- src/unsw/loopmania/Character.java
+++ src/unsw/loopmania/Character.java
@@ -15,8 +15,7 @@ import unsw.loopmania.items.WeaponStrategy;
  */
 public class Character extends MovingEntity {
     private int health;
-    // private final int damage; ??
-    private int damage;
+    private final int damage = 5;
     private ArrayList<Enemy> enemiesCurrentlyBattling;
     private WeaponStrategy weaponStrat;
     private BodyArmourStrategy bodyArmourStrat;
@@ -35,7 +34,6 @@ public class Character extends MovingEntity {
     public Character(PathPosition position) {
         super(position);
         this.health = 100;
-        this.damage = 5;
         this.enemiesCurrentlyBattling = new ArrayList<>();
         this.weaponStrat = new Melee();
         this.bodyArmourStrat = new Melee();
@@ -225,14 +223,14 @@ public class Character extends MovingEntity {
      * Restores Character's health to maximum
      */
     public void restoreHealthPoints() {
-        health = 100;
+        this.health = 100;
     }
 
     /**
      * @return true if character has full health
      */
     public boolean isFullHealth() {
-        return health == 100;
+        return this.health == 100;
     }
 
     @Override
