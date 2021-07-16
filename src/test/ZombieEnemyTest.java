18ba9701e1ef83b0c7f3ae0c4698c88018d14d5a
CyrusWilkie <james.wilkiec@gmail.com>
2021-07-16 21:11:49 +1000

    completed tests for zombie critical bite

--- src/test/ZombieEnemyTest.java
+++ src/test/ZombieEnemyTest.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 
 import org.junit.jupiter.api.Test;
 
+import unsw.loopmania.Ally;
 import unsw.loopmania.Character;
 import unsw.loopmania.enemies.ZombieEnemy;
 import unsw.loopmania.path.PathPosition;
@@ -27,5 +28,101 @@ public class ZombieEnemyTest {
         // 2 Being the equivalent of 1 tile
         assertEquals(3, newZombie.getAttackRadius());
         assertEquals(3, newZombie.getSupportRadius());
+    }
+
+    @Test
+    public void takeDamageTest() {
+        ZombieEnemy newZombie = new ZombieEnemy(null);
+
+        // Testing basic damage
+        newZombie.receiveAttack(10);
+        assertEquals(41, newZombie.getHealth());
+        newZombie.receiveAttack(10);
+        assertEquals(32, newZombie.getHealth());
+        newZombie.receiveAttack(20);
+        assertEquals(14, newZombie.getHealth());
+        newZombie.receiveAttack(0);
+        assertEquals(14, newZombie.getHealth());
+        newZombie.receiveAttack(32);
+        assertEquals(0, newZombie.getHealth());
+        newZombie.receiveAttack(5);
+        assertEquals(0, newZombie.getHealth());
+    }
+
+    @Test
+    public void giveDamageTest() {
+        // Tests that the zombie gives out correct damage in attacks
+        // Also ensures that zombie attacks slower than other enemies
+        Character newChar = new Character(null);
+        ZombieEnemy newZombie = new ZombieEnemy(null);
+
+        newZombie.launchAttack(newChar);
+        assertEquals(100, newChar.getHealth());
+        newZombie.launchAttack(newChar);
+        assertEquals(100, newChar.getHealth());
+        newZombie.launchAttack(newChar);
+        assertEquals(90, newChar.getHealth());
+        newZombie.launchAttack(newChar);
+        assertEquals(90, newChar.getHealth());
+        newZombie.launchAttack(newChar);
+        assertEquals(90, newChar.getHealth());
+        newZombie.launchAttack(newChar);
+        assertEquals(80, newChar.getHealth());
+    }
+
+    @Test
+    public void movementTest() {
+        // Tests that the slug moves around the map
+        PathPosition position = null;
+
+        try {
+            position = TestHelper.generatePathPosition("bin/test/Resources/world_with_twists_and_turns.json");
+        } catch (FileNotFoundException e) {
+            // Failed to generate PathPostion
+            assertTrue(false);
+        }
+
+        ZombieEnemy newZombie = new ZombieEnemy(position);
+
+        int initialX = newZombie.getX();
+        int initialY = newZombie.getY();
+        boolean moved = false;
+        int i = 0;
+
+        while (i < 100) {
+            newZombie.move();
+
+            if (initialX != newZombie.getX() || initialY != newZombie.getY()) {
+                moved = true;
+            }
+
+            i++;
+        }
+
+        assertTrue(moved);
+    }
+
+    @Test
+    public void specialAttackTest() {
+        // Testing that a vampire successfully creates another vampire
+        // using critical bite
+        boolean biteOccurred = false;
+
+        for (int i = 0; i < 100; i++) {
+            Character newChar = new Character(null);
+            newChar.addAlly(new Ally());
+            newChar.addAlly(new Ally());
+            ZombieEnemy newZombie = new ZombieEnemy(null);
+
+            newZombie.launchAttack(newChar);
+            newZombie.launchAttack(newChar);
+
+            if (newZombie.launchAttack(newChar)) {
+                biteOccurred = true;
+                assertEquals(0, newChar.getNumAllies());
+            }
+        }
+
+        assertEquals(true, biteOccurred);
     }
 }
