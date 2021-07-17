74224ceae29ab0a32e3badbe739a69203acd515d
CyrusWilkie <james.wilkiec@gmail.com>
2021-07-17 15:04:32 +1000

    implemented gold leaf goal

--- src/unsw/loopmania/goals/GoldBaseGoal.java
+++ src/unsw/loopmania/goals/GoldBaseGoal.java
@@ -1,5 +1,18 @@
 package unsw.loopmania.goals;
 
+import unsw.loopmania.LoopManiaWorld;
+
 public class GoldBaseGoal implements ComplexGoalComponent {
-    
+    private int achievementThreshold;
+
+    public GoldBaseGoal(int achievementThreshold) {
+        this.achievementThreshold = achievementThreshold;
+    }
+
+    public boolean achieved(LoopManiaWorld world) {
+        if (world.getGold() >= this.achievementThreshold)
+            return true;
+        else
+            return false;
+    }
 }
