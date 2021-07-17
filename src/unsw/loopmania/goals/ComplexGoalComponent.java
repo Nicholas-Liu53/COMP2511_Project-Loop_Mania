e24edada4650de360ef2b38c653c19c2757c01c0
CyrusWilkie <james.wilkiec@gmail.com>
2021-07-17 14:43:59 +1000

    created complex goal interface

---
+++ src/unsw/loopmania/goals/ComplexGoalComponent.java
@@ -0,0 +1,14 @@
+package unsw.loopmania.goals;
+
+import unsw.loopmania.LoopManiaWorld;
+
+public interface ComplexGoalComponent {
+    
+    /**
+     * Takes in world state and returns whether goals have been met
+     * 
+     * @param world
+     * @return true if the goal has been achieved
+     */
+    public boolean achieved(LoopManiaWorld world);
+}
