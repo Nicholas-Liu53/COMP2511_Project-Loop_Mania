f2e46649c6c9315552d5d7a1bf41df54ae9a3a83
CyrusWilkie <james.wilkiec@gmail.com>
2021-07-17 14:23:46 +1000

    added basic tests for goals

---
+++ src/test/GoalsTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class GoalsTest {
+    @Test
+    public void basicGoalsTest() {
+        return;
+    }
+}
