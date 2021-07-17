6ece59d77cbd7c5e34b30838e892720402d780fb
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2021-07-18 04:49:43 +0530

    implemented goalloader; needs improvement

--- src/unsw/loopmania/goalloader.java
+++ src/unsw/loopmania/goalloader.java
@@ -30,16 +30,41 @@ public class goalloader {
     }
 
     public void loadAllGoals() {
+        ComplexGoalComposite x = loadComplexGoal(null, this.json.getJSONObject("goal-condition"));
+        System.out.println(x);
+        System.out.println(x.getChildren());
     }
 
     /**
      * Parses the JSON to create a goal.
      */
     public ComplexGoalComposite loadComplexGoal(ComplexGoalComposite cg, JSONObject goalCondition) {
+        if (cg == null) {
+            if (goalCondition.getString("goal").equals("AND")) {
+                cg = new ComplexGoalComposite("AND");
+                loadSubGoals(cg, goalCondition.getJSONArray("subgoals"));
+            } else if (goalCondition.getString("goal").equals("OR")) {
+                cg = new ComplexGoalComposite("OR");
+                loadSubGoals(cg, goalCondition.getJSONArray("subgoals"));
+            }
+            
+            if (goalCondition.getString("goal").equals("cycles")) {
+                cg.add(new CyclesBaseGoal(goalCondition.getInt("quantity")));
+            } else if (goalCondition.getString("goal").equals("experience")) {
+                cg.add(new XpBaseGoal(goalCondition.getInt("quantity")));
+            } else if (goalCondition.getString("goal").equals("gold")) {
+                cg.add(new GoldBaseGoal(goalCondition.getInt("quantity")));
+            } else {
+                throw new RuntimeException("Goal object requires either a basic goal or an operator for subgoal.");
+            }
+
         return cg;
     }
 
     private void loadSubGoals(ComplexGoalComposite cg, JSONArray subgoals) {
+        for (int i = 0; i < subgoals.length(); i++) {
+            loadComplexGoal(cg, subgoals.getJSONObject(i));
+        }
     }
 
     public static void main(String args[]) {
