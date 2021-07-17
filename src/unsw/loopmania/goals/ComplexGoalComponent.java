b6b276d08a4df288cde82397598a5221982f9901
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2021-07-18 04:39:34 +0530

    modified the interface complexGoalComponent

--- src/unsw/loopmania/goals/ComplexGoalComponent.java
+++ src/unsw/loopmania/goals/ComplexGoalComponent.java
@@ -3,7 +3,7 @@ package unsw.loopmania.goals;
 import unsw.loopmania.LoopManiaWorld;
 
 public interface ComplexGoalComponent {
-    
+
     /**
      * Takes in world state and returns whether goals have been met
      * 
@@ -11,4 +11,6 @@ public interface ComplexGoalComponent {
      * @return true if the goal has been achieved
      */
     public boolean achieved(LoopManiaWorld world);
+
+    public int achievementThreshold();
 }
