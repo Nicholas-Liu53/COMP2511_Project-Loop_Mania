aa955e01f145e017f41ef2ef3c2092cf7d924868
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2021-07-18 10:33:29 +0530

    removing unused functions

---
+++ src/unsw/loopmania/items/RareItemStrategy.java
@@ -0,0 +1,15 @@
+package unsw.loopmania.items;
+
+/**
+ * Interface used for implementing armour items
+ */
+public interface RareItemStrategy {
+    /**
+     * Receive attack method for the armour, takes in damage received and returns
+     * damage received after armour defence is factored in
+     * 
+     * @param damage
+     * @return
+     */
+    public int receiveAttack(int damage);
+}
