cb76bc7e8fbf67e4a8f32908799262274a0f41b0
CyrusWilkie <james.wilkiec@gmail.com>
2021-07-18 12:23:17 +1000

    added additional JSON goals tests

--- src/test/GoalsTest.java
+++ src/test/GoalsTest.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.junit.Test;
 
 import unsw.loopmania.Character;
 import unsw.loopmania.LoopManiaWorld;
+import unsw.loopmania.goals.ComplexGoalComponent;
 import unsw.loopmania.goals.ComplexGoalComposite;
 import unsw.loopmania.goals.CyclesBaseGoal;
 import unsw.loopmania.goals.GoldBaseGoal;
@@ -220,6 +224,117 @@ public class GoalsTest {
 
         // Remove goal and test
         complexGoal.remove(goldGoal);
+        assertEquals(true, complexGoal.achieved(testWorld));
+    }
+
+    @Test
+    public void parserTestOR() {
+        // Testing that goals can be successfully parsed from JSON
+        
+        JSONObject json = null;
+        
+        // Getting JSON
+        try {
+            json = new JSONObject(new JSONTokener(new FileReader("bin/test/Resources/complex_goal3.json")));
+        } catch (FileNotFoundException e) {
+            try {
+                json = new JSONObject(new JSONTokener(new FileReader("src/test/Resources/complex_goal3.json")));
+            } catch (FileNotFoundException ee) {
+                assertEquals(true, false);
+            }
+        }
+
+        // Creating goal
+        ComplexGoalComponent complexGoal = TestHelper.loadGoals(json.getJSONObject("goal-condition"));
+
+        PathPosition position = null;
+
+        try {
+            position = TestHelper.generatePathPosition("bin/test/Resources/world_with_twists_and_turns.json");
+        } catch (FileNotFoundException e) {
+            // Using gradle, different path is needed
+            try {
+                position = TestHelper.generatePathPosition("src/test/Resources/world_with_twists_and_turns.json");
+            } catch (FileNotFoundException ee) {
+                // Failed to generate PathPostion
+                assertTrue(false);
+            }
+        }
+
+        //Constructing world
+        LoopManiaWorld testWorld = TestHelper.generateWorld();
+        Character mainChar = new Character(position);
+        testWorld.setCharacter(mainChar);
+        testWorld.setGoals(complexGoal);
+
+        assertEquals(false, complexGoal.achieved(testWorld));
+
+        // Give xp and test
+        mainChar.giveExperiencePoints(100);
+        assertEquals(false, complexGoal.achieved(testWorld));
+
+        // Give gold and test
+        testWorld.giveGold(100);
+        assertEquals(true, complexGoal.achieved(testWorld));
+
+        mainChar.giveExperiencePoints(1234567);
+        assertEquals(true, complexGoal.achieved(testWorld));
+    }
+
+    @Test
+    public void parserTestAND() {
+        // Testing that goals can be successfully parsed from JSON
+        
+        JSONObject json = null;
+        
+        // Getting JSON
+        try {
+            json = new JSONObject(new JSONTokener(new FileReader("bin/test/Resources/complex_goal0.json")));
+        } catch (FileNotFoundException e) {
+            try {
+                json = new JSONObject(new JSONTokener(new FileReader("src/test/Resources/complex_goal0.json")));
+            } catch (FileNotFoundException ee) {
+                assertEquals(true, false);
+            }
+        }
+
+        // Creating goal
+        ComplexGoalComponent complexGoal = TestHelper.loadGoals(json.getJSONObject("goal-condition"));
+
+        PathPosition position = null;
+
+        try {
+            position = TestHelper.generatePathPosition("bin/test/Resources/world_with_twists_and_turns.json");
+        } catch (FileNotFoundException e) {
+            // Using gradle, different path is needed
+            try {
+                position = TestHelper.generatePathPosition("src/test/Resources/world_with_twists_and_turns.json");
+            } catch (FileNotFoundException ee) {
+                // Failed to generate PathPostion
+                assertTrue(false);
+            }
+        }
+
+        //Constructing world
+        LoopManiaWorld testWorld = TestHelper.generateWorld();
+        Character mainChar = new Character(position);
+        testWorld.setCharacter(mainChar);
+        testWorld.setGoals(complexGoal);
+
+        assertEquals(false, complexGoal.achieved(testWorld));
+
+        // Give xp and test
+        mainChar.giveExperiencePoints(1234567);
+        assertEquals(false, complexGoal.achieved(testWorld));
+
+        // Give gold and test
+        testWorld.giveGold(900001);
+        assertEquals(false, complexGoal.achieved(testWorld));
+
+        // Do cycles and test
+        for (int i = 0; i < 1000; i++) {
+            testWorld.runTickMoves();
+        }
         assertEquals(true, complexGoal.achieved(testWorld));
     }
 }
