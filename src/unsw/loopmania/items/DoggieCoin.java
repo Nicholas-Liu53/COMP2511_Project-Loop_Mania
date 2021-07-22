f7aef2041e508b5da86fbf31f13af441762c6eab
CyrusWilkie <james.wilkiec@gmail.com>
2021-07-22 18:42:46 +1000

    implemented doggie coin backend

---
+++ src/unsw/loopmania/items/DoggieCoin.java
@@ -0,0 +1,29 @@
+package unsw.loopmania.items;
+
+import java.util.Random;
+
+import org.javatuples.Pair;
+
+import unsw.loopmania.path.PathPosition;
+
+public class DoggieCoin extends Item {
+    public DoggieCoin(Pair<Integer, Integer> position) {
+        super(position);
+    }
+
+    public DoggieCoin(PathPosition position) {
+        super(position);
+    }
+
+    public DoggieCoin() {
+        super(new Pair<Integer, Integer>(1, 2));
+    }
+
+    public int getSellPrice() {
+        return (50 + new Random().nextInt(950));
+    }
+
+    public int getPurchasePrice() {
+        return 0;
+    }
+}
