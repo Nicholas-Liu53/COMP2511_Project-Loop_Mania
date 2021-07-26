82c2ee539b04279f5ff04141c5fbc02dc901f4b8
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2021-07-26 15:07:53 +0530

    created anduril class

---
+++ src/unsw/loopmania/items/Anduril.java
@@ -0,0 +1,13 @@
+package unsw.loopmania.items;
+
+import org.javatuples.Pair;
+
+import unsw.loopmania.character.Character;
+import unsw.loopmania.enemies.Enemy;
+
+/**
+ * represents an equipped or unequipped anduril in the backend world
+ */
+public class Anduril extends Item implements WeaponStrategy {
+
+}
