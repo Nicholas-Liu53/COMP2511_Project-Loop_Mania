aa09974aa8d26b1e38cb725735dc8346a3e903fa
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2021-07-26 15:43:24 +0530

    implemented tree stump

--- src/unsw/loopmania/items/TreeStump.java
+++ src/unsw/loopmania/items/TreeStump.java
@@ -12,4 +12,10 @@ public class TreeStump extends Armour implements ShieldStrategy {
         super(new Pair<Integer, Integer>(1, 2));
         this.damageReductionFactor = 0.6;
     }
+
+    public int receiveAttack(int damage) {
+        // TreeStump provides 3 defence
+        int recvDamage = (int) (damage * 0.45);
+        return recvDamage;
+    }
 }
